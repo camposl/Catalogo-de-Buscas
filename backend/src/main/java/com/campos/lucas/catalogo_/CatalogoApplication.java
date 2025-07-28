2408f110a122aa27e42fdb6857624e78649c246e
camposl <lucascampos25@gmail.com>
2025-07-28 13:17:06 -0300

    Project Created

---
+++ backend/src/main/java/com/campos/lucas/catalogo_/CatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.campos.lucas.catalogo_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoApplication.class, args);
+	}
+
+}
